ab7dd64dafa4c22b9035432d8db4415af8889bb0
nabab <thomas.nabet@gmail.com>
2016-05-02 07:55:45 +0200

    Added shortcut deletion

---
+++ src/mvc/public/shortcuts/delete.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Describe what it does to show you're not that dumb!
+ *
+ **/
+
+/** @var $this \bbn\mvc\controller */
+if ( isset($this->post['id']) ){
+  $menu = new \bbn\appui\menu($this->inc->options);
+  $this->obj->success = $menu->remove_shortcut($this->post['id'], $this->inc->pref);
+}
