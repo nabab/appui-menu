d52f2095d9af58b4ee3502fefa5e0f87b220c12d
nabab <thomas.nabet@gmail.com>
2017-03-17 04:25:13 +0100

    Added a data controller

---
+++ src/mvc/model/data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BBN
+ * Date: 14/03/2017
+ * Time: 16:06
+ */
---
+++ src/mvc/public/data.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BBN
+ * Date: 14/03/2017
+ * Time: 16:05
+ */
+
+$ctrl->obj->data = $ctrl->get_object_model('./user_menu');
