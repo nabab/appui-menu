285eeec7c87258766b5aad3ee8998a68a08c3817
nabab <thomas.nabet@gmail.com>
2017-08-02 17:35:28 +0200

    Added a transform parameter to custom_tree

--- src/mvc/model/user_menu.php
+++ src/mvc/model/user_menu.php
@@ -5,9 +5,9 @@
  **/
 
 /** @var $model \bbn\mvc\model*/
-$menu = new \bbn\appui\menus($model->inc->options,  $model->data['prepath'] ?: false);
+$menu = new \bbn\appui\menus($model->inc->options,  $model->data['prepath'] ?? false);
 if ( isset($model->inc->pref) ){
-  return $menu->custom_tree('default', $model->inc->pref, $model->data['prepath'] ?: false, [
+  return $menu->custom_tree('default', $model->inc->pref, $model->data['prepath'] ?? false, [
     'text' => 'title',
     'items' => 'children'
   ]);
