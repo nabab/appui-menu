198f320fbdbc053445e484916c9faa5d5c12286e
Thomas Nabet <thomas.nabet@gmail.com>
2020-10-01 04:37:40 +0200

    Code formatting

--- src/mvc/model/actions/item/order.php
+++ src/mvc/model/actions/item/order.php
@@ -1,17 +1,15 @@
 <?php
-if (
-  !empty($model->data['id_menu']) &&
-  \bbn\str::is_uid($model->data['id_menu']) &&
-  !empty($model->data['id']) &&
-  \bbn\str::is_uid($model->data['id']) &&
-  !empty($model->data['num']) &&
-  ($menu = $model->inc->pref->get($model->data['id_menu']))
-){
-  if (
-    !empty($menu['public']) &&
-    !$model->inc->user->is_admin() &&
-    !$model->inc->user->is_dev()
-  ){
+
+/**
+ * @var bbn\mvc\model $model
+ **/
+if ($model->has_data(['id_menu', 'id', 'num'], true)
+    && ($menu = $model->inc->pref->get($model->data['id_menu']))
+) {
+  if (!empty($menu['public'])
+      && !$model->inc->user->is_admin()
+      && !$model->inc->user->is_dev()
+  ) {
     return ['success' => false];
   }
   return ['success' => $model->inc->menu->order($model->data['id'], $model->data['num'])];
