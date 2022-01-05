b3767147880457abdc9f2a1725c938072cd2e58e
nabab <thomas.nabet@gmail.com>
2022-01-05 14:31:01 +0100

    Fix in menu

--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -1,6 +1,8 @@
 <?php
 /** @var $ctrl \bbn\Mvc\Controller */
-$ctrl->addInc('menu', new \bbn\Appui\Menu());
+if (!isset($ctrl->inc->menu)) {
+  $ctrl->addInc('menu', new \bbn\Appui\Menu());
+}
 if ( !defined('APPUI_MENU_ROOT') ){
   define('APPUI_MENU_ROOT', $ctrl->pluginUrl('appui-menu').'/');
 }
--- src/mvc/public/shortcuts/_ctrl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Describe what it does!
- *
- **/
-
-/** @var $ctrl \bbn\Mvc\Controller */
-
-$ctrl->addInc('menu', new \bbn\Appui\Menu($ctrl->inc->options));
