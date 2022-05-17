31f9433074426ed8837e15dbf39b23a505d634b5
nabab <thomas.nabet@gmail.com>
2022-05-18 01:27:08 +0200

    Fix in insert shortcut, now from tabs

--- src/mvc/model/shortcuts/insert.php
+++ src/mvc/model/shortcuts/insert.php
@@ -4,10 +4,16 @@
  *
  **/
 
+use bbn\X;
 /** @var $this \bbn\Mvc\Model*/
 $res = ['success' => false];
-if ( isset($model->data['id']) ){
-  if ( $res['id'] = $model->inc->menu->addShortcut($model->data['id'], $model->inc->pref) ){
+if (!isset($model->data['id']) && $model->hasData(['url', 'text', 'icon'], true)) {
+  if ($res['id'] = $model->inc->menu->addShortcutByUrl($model->data['url'], $model->data['text'], $model->data['icon'])) {
+    $res['success'] = true;
+  }
+}
+elseif (isset($model->data['id'])){
+  if ($res['id'] = $model->inc->menu->addShortcut($model->data['id'])) {
     $res['success'] = true;
   }
 }
